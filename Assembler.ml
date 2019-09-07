050fe7d1e5827df1560cf3b5d45c4ee6ebfef313
Atte Torri <atte@torri.be>
2019-09-07 21:56:12 +0200

    new stuff

--- Assembler.ml
+++ Assembler.ml
@@ -1,6 +1,21 @@
 (* Made by Atte Torri on Friday 6 September 2019 *)
 
+let encode i =
+  match List.hd i with
+  | "NOP" -> 0
+  | "EXIT" -> 1 lsl 26
+  | _ -> 0
+
 let () =
+  let out = open_out_bin Sys.argv.(2) in
   let file = Sys.argv.(1) in
   let f = open_in file in
+  try
+    while true do
+      let instr = Str.split (Str.regexp "[ \t]+") (input_line f) in
+      let instr = List.filter (fun e -> e<>"") instr in
+      output_binary_int out (encode instr)
+    done
+  with
+  | End_of_file -> close_in f; close_out out
   
--- VM.ml
+++ VM.ml
@@ -1,7 +1,7 @@
 (* Made by Atte Torri on Friday 6 September 2019 *)
 
-let memory = Array.make 65536
-let registers = Array.make 32
+let memory = Array.make 65536 0
+let registers = Array.make 32 0
 let pc = ref 0
 let branch_flag = ref false
 
@@ -10,7 +10,7 @@ let r_dest i = (i lsr 21) mod 32 (* 5 bits for each register *)
 let r_arg1 i = (i lsr 16) mod 32
 let r_arg2 i = (i lsr 11) mod 32
 let imm i = i mod 2097152 (* 21 bits for immediate *)
-                
+
 let execute i =
   match op_code i with
   | 0 -> (* NOP *)
@@ -22,17 +22,23 @@ let execute i =
   | 3 -> (* STORE *)
      memory.(registers.(r_dest i)) <- registers.(r_arg1 i)
   | 4 -> (* PRINT *)
-     Printf.printf (char_of_int registers.(r_arg1 i))
-     
+     print_char (char_of_int registers.(r_arg1 i))
+  | 5 -> (* ADD *)
+     registers.(r_dest i) <- registers.(r_arg1 i) + registers.(r_arg2 i)
+  | 6 -> (* JUMP *)
+     branch_flag := true;
+     pc := registers.(r_arg1 i)
+  | 7 -> (* CONST *)
+     registers.(r_dest i) <- imm i
   | _ -> (* UNASSIGNED *)
      ()
        
 let () =
   let file = Sys.argv.(1) in
-  let f = open_in file in
+  let f = open_in_bin file in
   try
     while true do
-      let instr = int_of_string (input_line f) in
+      let instr = input_binary_int f in
       execute instr;
       if not (!branch_flag) then
         incr pc;
